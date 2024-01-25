848ab6494f3b0e82a9552360ac3f6fd829f1fbb4
Peter van Hardenberg <pvh@pvh.ca>
2024-01-24 16:19:42 -0800

    storage adapter

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,14 @@ import './index.css'
 
 import { Repo } from "@automerge/automerge-repo"
 import { BrowserWebSocketClientAdapter } from '@automerge/automerge-repo-network-websocket'
+import { IndexedDBStorageAdapter } from "@automerge/automerge-repo-storage-indexeddb"
+
 import { RepoContext } from "@automerge/automerge-repo-react-hooks"
 
-const repo = new Repo({network: [new BrowserWebSocketClientAdapter("wss://sync.automerge.org")]}) 
+const repo = new Repo({
+  network: [new BrowserWebSocketClientAdapter("wss://sync.automerge.org")], 
+  storage: new IndexedDBStorageAdapter("tldraw-demo")
+}) 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
